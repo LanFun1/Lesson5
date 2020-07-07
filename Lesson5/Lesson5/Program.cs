7e8fcdd77289360511e7ad0412f96c4ea334f8c2
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-07 19:48:33 +0300

    Add files via upload

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Кем вы работаете?");
+            string work = Console.ReadLine();
+            Console.WriteLine("Кто вы по знаку зодиака?");
+            string sign = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}. Вам {age} год(года,лет). Вы {work}. Ваш знак зодиака {sign}");
+        }
+    }
+}
